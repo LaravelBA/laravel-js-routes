120766ddeaac749b176028ca32d7ba96f2d5b3bd
Guido Contreras Woda <guiwoda@gmail.com>
2016-05-24 10:51:56 -0300

    Revisited node dev dependencies and travis configuration

--- tests/jasmine/spec/RouterSpec.js
+++ tests/jasmine/spec/RouterSpec.js
@@ -15,47 +15,47 @@ describe("Router", function() {
   ];
 
   describe("When Router.route() it's called", function() {
-    it('Should replace {params} in URI', function(done) {
+    it('Should replace {params} in URI', function() {
       expect(Router.route('user.show', {id: 1})).toEqual('http://domain.tld/user/1');
     });
 
-    it('Should replace {params} in URI', function(done) {
+    it('Should replace {params} in URI', function() {
       expect(Router.route('user.edit', {id: 1})).toEqual('http://domain.tld/user/1/edit');
     });
 
-    it('Should not replace wrong {params} in URI', function(done) {
+    it('Should not replace wrong {params} in URI', function() {
       expect(Router.route('user.show', {wrong: 1})).toEqual('http://domain.tld/user/{id}?wrong=1');
     });
 
-    it('Should return undefined for unexistent routes', function(done) {
+    it('Should return undefined for unexistent routes', function() {
       expect(Router.route('gusfraba', {id: 1})).toBeUndefined();
     });
 
-    it('Should return root URL', function(done) {
+    it('Should return root URL', function() {
       expect(Router.getRootUrl()).toEqual('http://domain.tld');
     });
 
-    it('Should append query string if there is no parameter in URI', function(done) {
+    it('Should append query string if there is no parameter in URI', function() {
       expect(Router.route('search.index', {q: 'John Doe'})).toEqual('http://domain.tld/search?q=John%20Doe');
     });
 
-    it('Should append query string and replace URI params', function(done) {
+    it('Should append query string and replace URI params', function() {
       expect(Router.route('search.byType', {type: 'people', q: 'john'})).toEqual('http://domain.tld/search/people?q=john');
     });
 
-    it('Should append many query string params', function(done) {
+    it('Should append many query string params', function() {
       expect(Router.route('search.byType', {type: 'people', q: 'john', orderBy: 'asc'})).toEqual('http://domain.tld/search/people?q=john&orderBy=asc');
     });
 
-    it('Should prevent many trailing slashes at the end', function(done) {
+    it('Should prevent many trailing slashes at the end', function() {
       expect(Router.route('home')).toEqual('http://domain.tld');
     });
 
-    it('Should account for optional parameters of routes', function(done) {
+    it('Should account for optional parameters of routes', function() {
       expect(Router.route('cart.add', {article: 'foo', count: 2})).toEqual('http://domain.tld/cart/add/foo/2');
     });
 
-    it('Should ignore unused optional parameters of routes', function(done) {
+    it('Should ignore unused optional parameters of routes', function() {
       expect(Router.route('cart.add', {article: 'foo'})).toEqual('http://domain.tld/cart/add/foo');
     });
   });
